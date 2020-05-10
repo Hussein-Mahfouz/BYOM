a78fd2943ea0fe372ab4474ced134206db402166
Hussein Mahfouz <husseinmahfouz93@gmail.com>
2020-05-10 19:00:23 +0100

    different gif

--- Sport/olympics.R
+++ Sport/olympics.R
@@ -9,15 +9,17 @@ library(gganimate)
 library(maps)
 library(transformr)
 
+# Olympic Data
+# https://docs.google.com/spreadsheets/d/1zeeZQzFoHE2j_ZrqDkVJK9eF7OH1yvg75c8S-aBcxaU/edit#gid=322436777
+
 data = read_csv("Summer_Olympic_medallists_1896_2008.csv", skip = 4)
 # read in world map as sf
 world = st_read("world.geo.json")
 #plot to check 
-plot(st_geometry(world))
+#plot(st_geometry(world))
 
 #subset only necessary columns
 world <- world[, c("sovereignt", "iso_a3")]
-plot(st_geometry(world))
 
 # GET STATS 
 
@@ -27,11 +29,12 @@ calculate_mode <- function(x) {
   uniqx[which.max(tabulate(match(x, uniqx)))]
 }
 
-
+# For each year, get country with most medals and the matching no of medals
 year_best = data %>% group_by(Edition) %>%
           summarise(best_country = calculate_mode(NOC),  #use function above to get most repeated country
                    medals = length(which(NOC==calculate_mode(NOC)))) # get count of medals for best country
 
+# change names because the ISO standard is different to the data found
 year_best$best_country[year_best$best_country=='GRE'] <- 'GRC'
 year_best$best_country[year_best$best_country=='URS'] <- 'RUS'
 year_best$best_country[year_best$best_country=='GER'] <- 'DEU'
@@ -42,22 +45,23 @@ x <- merge(world, year_best)
 # filter to get only the rows where the spatial data matches the winning country
 map_winners <- x[x$iso_a3==x$best_country, ]
 
-# create new column as ggplot title https://stackoverflow.com/questions/53864892/gganimate-include-additional-variable-other-than-states-level-variable-or-frame
+# create new column to display as ggplot title 
+# cannot use different columns as title / subtitle. It will only display the column passed to transition_states()
+# https://stackoverflow.com/questions/53864892/gganimate-include-additional-variable-other-than-states-level-variable-or-frame
 map_winners <- mutate(map_winners, title_var2 = factor(paste(Edition, sovereignt, medals, sep=" - "), levels = paste(Edition, sovereignt, medals, sep=" - ")))
-
-
-plot(st_geometry(map_winners))
 
 p <- ggplot(data = world) +
       geom_sf() +
       geom_sf(data = map_winners, aes(fill = medals)) +
-      theme(plot.title = element_text(size = 40, face = "bold")) +
+      theme(plot.title = element_text(size = 30, face = "bold")) +
       labs(title = "{closest_state} Medals") +
       scale_fill_viridis_c(trans = "sqrt", alpha = .4) + 
       transition_states(map_winners$title_var2, state_length = 15) +
       enter_fade() +
       exit_fade()
 
-animate(p, height = 560, width = 990, duration = 52, nframes = 26)
-anim_save("olympics2.gif")
+# duration/nframes should be the time spent on each frame
+animate(p, height = 600, width = 920, duration = 52, nframes = 26)
+# save
+anim_save("olympics3.gif")
 
